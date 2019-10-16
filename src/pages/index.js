419d36a8368cf13a6c27af28c1be1e52a7421b3e
Dina <dnafication@users.noreply.github.com>
2019-10-16 15:05:55 +1100

    updated logo and hero

--- src/pages/index.js
+++ src/pages/index.js
@@ -22,7 +22,7 @@ const features = [
         Docusaurus was designed from the ground up to be easily installed and
         used to get your website up and running quickly.
       </>
-    ),
+    )
   },
   {
     title: <>Focus on What Matters</>,
@@ -32,7 +32,7 @@ const features = [
         Docusaurus lets you focus on your docs, and we&apos;ll do the chores. Go
         ahead and move your docs into the <code>docs</code> directory.
       </>
-    ),
+    )
   },
   {
     title: <>Powered by React</>,
@@ -42,28 +42,34 @@ const features = [
         Extend or customize your website layout by reusing React. Docusaurus can
         be extended while reusing the same header and footer.
       </>
-    ),
-  },
+    )
+  }
 ];
 
 function Home() {
   const context = useDocusaurusContext();
-  const {siteConfig = {}} = context;
+  const { siteConfig = {} } = context;
   return (
     <Layout
-      title={`Hello from ${siteConfig.title}`}
-      description="Description will go into a meta tag in <head />">
+      title={`${siteConfig.title} | ${siteConfig.tagline}`}
+      description="Description will go into a meta tag in <head />"
+    >
       <header className={classnames('hero hero--primary', styles.heroBanner)}>
         <div className="container">
-          <h1 className="hero__title">{siteConfig.title}</h1>
-          <p className="hero__subtitle">{siteConfig.tagline}</p>
+          <h1 className="hero__title">{siteConfig.tagline}</h1>
+          <p className="hero__subtitle">
+            Benefit from all the features of MySQL while using RocksDB as
+            backend storage
+          </p>
+
           <div className={styles.buttons}>
             <Link
               className={classnames(
                 'button button--outline button--secondary button--lg',
-                styles.getStarted,
+                styles.getStarted
               )}
-              to={withBaseUrl('docs/doc1')}>
+              to={withBaseUrl('docs/doc1')}
+            >
               Get Started
             </Link>
           </div>
@@ -74,10 +80,11 @@ function Home() {
           <section className={styles.features}>
             <div className="container">
               <div className="row">
-                {features.map(({imageUrl, title, description}, idx) => (
+                {features.map(({ imageUrl, title, description }, idx) => (
                   <div
                     key={idx}
-                    className={classnames('col col--4', styles.feature)}>
+                    className={classnames('col col--4', styles.feature)}
+                  >
                     {imageUrl && (
                       <div className="text--center">
                         <img
